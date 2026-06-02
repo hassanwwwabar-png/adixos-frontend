e0ff33d4a923bdfec80729687ee317e6ae038d1a
hassan abagh <hassanwwwabar@gmail.com>
2026-06-02 15:42:52 +0100

    Add Next.js proxy for HTTPS to HTTP routing

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        // عندما يطلب الموقع أي رابط يبدأ بـ /api/
+        source: '/api/:path*',
+        // قم بتمريره في الخفاء إلى السيرفر الألماني
+        destination: 'http://2.24.14.60:8000/api/:path*', 
+      },
+    ];
+  },
 };
 
 export default nextConfig;
